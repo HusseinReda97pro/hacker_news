4cf437f3ec1545acfbb5cb64eda0e8bff057af45
HusseinReda97pro <pro.hussein.reda@gmail.com>
2024-09-26 11:23:05 +0300

    Initial Commit

---
+++ hacker_news/hacker_newsApp.swift
@@ -0,0 +1,17 @@
+//
+//  hacker_newsApp.swift
+//  hacker_news
+//
+//  Created by Hussein Reda on 26/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct hacker_newsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
